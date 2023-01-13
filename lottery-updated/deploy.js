54550d77c5a1ee2257dbf207e5e291e0f8506f4f
Sriram <sriss2003@gmail.com>
2023-01-14 01:33:56 +0530

    uploaded all files

---
+++ lottery-updated/deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  
+  "false reduce faith sleep mask bullet common bulb glimpse alcohol void cluster",
+ "https://goerli.infura.io/v3/627b7f259a2444c8a5b17db2303ebf25"
+  
+  
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
